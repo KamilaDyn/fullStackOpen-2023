3e4b2e8e6d0a5aa66543ab60734a875afc009031
Kamila <kamila.dynysiuk@gmail.com>
2023-05-18 18:23:22 +0200

    rename props, map parts, rename key in course object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,33 +13,43 @@ const Part = ({ name, exercises }) => {
 const Content = ({ parts }) => {
   return (
     <>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
+      {parts.map((item) => (
+        <Part key={item.name} name={item.name} exercises={item.exercises} />
+      ))}
     </>
   );
 };
 
-const Total = ({ total }) => {
-  return <p>Number of exercises {total[0] + total[1] + total[2]}</p>;
+const Total = ({ parts }) => {
+  return <p>Number of exercises {parts[0] + parts[1] + parts[2]}</p>;
 };
 
 const App = () => {
   const course = {
-    title: "Half Stack application development",
+    name: "Half Stack application development",
     parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 },
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
     ],
   };
-  const exercisesTotal = course.parts.map((item) => item["exercises"]);
+
+  const countParts = course.parts.map((item) => item["exercises"]);
 
   return (
     <div>
-      <Header course={course.title} />
+      <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total total={exercisesTotal} />
+      <Total parts={countParts} />
     </div>
   );
 };
