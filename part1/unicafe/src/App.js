abeed4766293a1c8c378ce1d628fd041d9928d40
Kamila <kamila.dynysiuk@gmail.com>
2023-05-19 20:01:54 +0200

    add part second , create app, add componnets for button display results

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+const Header = ({ title }) => {
+  return <h2>{title}</h2>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+const Statistic = ({ statistic }) => {
+  return (
+    <div>
+      {statistic.map((item) => (
+        <StatisticLine key={item.name} text={item.name} value={item.state} />
+      ))}
+    </div>
+  );
+};
+
+const Button = ({ name, handleClick }) => {
+  return <button onClick={handleClick}>{name}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const allFeedback = good + neutral + bad;
+  const averageFeedback = (good - bad) / allFeedback;
+  const positiveFeedback = (good / allFeedback) * 100;
+  const course = {
+    title: "give feedback",
+    feedback: [
+      { name: "good", state: good },
+      { name: "neutral", state: neutral },
+      { name: "bad", state: bad },
+      { name: "all", state: allFeedback },
+      { name: "average", state: averageFeedback },
+      { name: "positive", state: `${positiveFeedback}%` },
+    ],
+    statistic: "statistic",
+  };
+
+  const buttons = [
+    { name: "good", handleFunction: () => setGood(good + 1) },
+    { name: "neutral", handleFunction: () => setNeutral(neutral + 1) },
+    { name: "bad", handleFunction: () => setBad(bad + 1) },
+  ];
+
+  return (
+    <div>
+      <Header title={course.title} />
+      {buttons.map((button) => (
+        <Button
+          key={button.name}
+          name={button.name}
+          handleClick={button.handleFunction}
+        />
+      ))}
+      <Header title={course.statistic} />
+      {allFeedback === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <Statistic statistic={course.feedback} />
+      )}
+    </div>
+  );
+};
+
+export default App;
