14ab602f156d879733021b5973d3ac72136eb255
Kamila <kamila.dynysiuk@gmail.com>
2023-05-24 09:57:22 +0200

    remove useeffect

--- part2/2.6-2.10/src/App.js
+++ part2/2.6-2.10/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import { mocData } from "./data";
@@ -6,19 +6,11 @@ import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState(mocData);
-  const [filteredPersons, setFilteredPersons] = useState(persons);
   const [filteredName, setFilteredName] = useState("");
 
   const changeFilterName = (event) => {
     setFilteredName(event.target.value);
   };
-
-  useEffect(() => {
-    const search = persons.filter((person) =>
-      person.name.toLocaleLowerCase().includes(filteredName)
-    );
-    setFilteredPersons(search);
-  }, [filteredName, persons]);
 
   return (
     <div>
@@ -26,7 +18,11 @@ const App = () => {
       <h2>Phonebook</h2>
       <PersonForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      <Persons persons={filteredPersons} />
+      <Persons
+        persons={persons.filter((person) =>
+          person.name.toLocaleLowerCase().includes(filteredName)
+        )}
+      />
     </div>
   );
 };
