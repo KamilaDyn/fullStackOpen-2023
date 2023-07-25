826a5944286a91e7c49a07446a85476dc38853d8
Kamila <kamila.dynysiuk@gmail.com>
2023-07-25 18:19:55 +0200

    add cors origin

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 app.use(morgan("tiny"));
 app.use(express.static("build"));
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
+
 app.use(
   morgan(":method :url :status :response-time ms - :res[content-length] :body")
 );
