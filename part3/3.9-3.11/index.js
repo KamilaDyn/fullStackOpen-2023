229f94e86d116bbc018e8b80dd137cd0f31411d8
Kamila <kamila.dynysiuk@gmail.com>
2023-07-26 14:18:20 +0200

    add steps 8-11, cors, build app

---
+++ part3/3.9-3.11/index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("build"));
+app.use(cors());
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length] :body")
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov2",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+const generatePersonId = () => {
+  return persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+};
+//step1
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.end(JSON.stringify(persons));
+// });
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const singlePerson = persons.find((person) => person.id === id);
+  if (singlePerson) {
+    response.json(singlePerson);
+  } else {
+    response.status(400).json({
+      error: "content missing",
+    });
+  }
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  const { name, number } = body;
+
+  if (!name || !number) {
+    return response.status(400).json({
+      error: "The name or number is missing",
+    });
+  } else if (persons.find((person) => person.name === name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = { name: name, number: number, id: generatePersonId };
+  persons = persons.concat(person);
+  response.json(person);
+});
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+app.get("/info", (request, response) => {
+  const time = new Date();
+  response.send(
+    `<p>Phone book has  info for ${persons.length} people</p><p>${time}</p>`
+  );
+  response.json(persons);
+});
+app.use(morgan("body"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
