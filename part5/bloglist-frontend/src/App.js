ffc41a5b97cd1b711b02942467d35e2e4ff642e3
Kamila <kamila.dynysiuk@gmail.com>
2023-08-11 16:10:54 +0200

    add ex 5.10 sort blog array

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -117,13 +117,15 @@ const App = () => {
             <BlogForm addBlog={addBlog} />
           </Toggleable>
 
-          {blogs.map((blog) => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              handleLikeChange={handleLikeChange}
-            />
-          ))}
+          {blogs
+            .map((blog) => (
+              <Blog
+                key={blog.id}
+                blog={blog}
+                handleLikeChange={handleLikeChange}
+              />
+            ))
+            .sort((a, b) => b.likes - a.likes)}
         </>
       )}
     </div>
